9e84c4bbc87f2bf2493afb332c9451ff73b2bf9a
7e5h <sambritton212@gmail.com>
2016-10-02 10:34:50 -0500

    layout and formatting changes

--- app/src/main/java/com/techacademy/demomaps/MainActivity.java
+++ app/src/main/java/com/techacademy/demomaps/MainActivity.java
@@ -1,5 +1,6 @@
 package com.techacademy.demomaps;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
     }
     public void mapView(View view){
         Intent i = new Intent(this, MapsActivity.class);
